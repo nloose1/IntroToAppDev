a5fbdc19090a01aa99464c9fbb50d421cfbd114c
NLoose15 <nickloose@hotmail.com>
2019-09-06 16:37:03 -0600

    create fence panel class file

---
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //default for all classes is private
+    //if an outside user of the class is to have access to the class then you need to make that class public
+    public class FencePane
+    {
+    }
+}
