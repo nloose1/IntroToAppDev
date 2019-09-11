e5a39e2ee1ededba817dae75b85a97e5afdca569
NLoose1 <nickloose@hotmail.com>
2019-09-11 16:46:56 -0600

    Create Estimate

---
+++ OOPsSolution/OOPsReview/Estimate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    class Estimate
+    {
+        public double TotalPrice { get; private set; }
+        public double LinearLength { get; set; }
+
+        public FencePanel Panel { get; set; }
+
+        public List<FenceGate> Gates { get; set; }
+
+        public double CalculateTotalPrice()
+        {
+            //assumption you would put appropriate validation at required points in you logic to ensure data is present before using 
+            double numberofpanels = Panel.EstimatedNumberOfPanels(LinearLength);
+            if((int)(numberofpanels * 10.0) > ((int)numberofpanels * 10))
+            {
+                numberofpanels = (int)numberofpanels + 1;
+            }
+            if (Panel.Price == null)
+            {
+                throw new Exception("Panel price is needed to calculate estimate");
+            }
+            else
+            {
+                TotalPrice += numberofpanels * (double)Panel.Price;
+                foreach(var item in Gates)
+                {
+                    TotalPrice += item.GatePrice;
+                }
+            }
+            return TotalPrice;
+        }
+    }
+}
