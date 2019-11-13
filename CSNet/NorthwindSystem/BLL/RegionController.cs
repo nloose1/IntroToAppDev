17b19eaffa7c693dc2be1be3246cfe330026c50d
NLoose1 <nickloose@hotmail.com>
2019-11-13 16:40:36 -0700

    bll stuff

---
+++ CSNet/NorthwindSystem/BLL/RegionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using NorthwindSystem.Data;
+using NorthwindSystem.DAL;
+#endregion
+
+namespace NorthwindSystem.BLL
+{
+    public class RegionController
+    {
+        //EntityFramework realizes that sertain requests for data are common
+        //EntityFramework has created methods that can be called to do the common requests
+
+        //method that will return all Region records
+        public List<Region> Regions_List()
+        {
+            //create an instance of the context class that will handle the data request
+            //Most of CRUD requires using a transaction
+            //to ensure that your data request is handled as a transaction we will encase all controller action within a transaction
+            using(var context = new NorthwindContext())
+            {
+                return context.Regions.ToList();
+            }
+        }
+
+
+        //method that will return a specific Region record based on its primary key
+
+        public Region Regions_FindByID(int regionid)
+        {
+            //transaction code
+            using (var context = new NorthwindContext())
+            {
+                return context.Regions.Find(regionid);
+            }
+        }
+
+    }
+}
---
+++ CSNet/WebApp/SamplePages/SimpleQuery.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional namespaces
+using NorthwindSystem.BLL;
+using NorthwindSystem.Data;
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SimpleQuery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //empty old message
+            MessageLabel.Text = "";
+        }
+
+        protected void Fetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(RegionIDArg.Text))
+            {
+                MessageLabel.Text = "Enter a region id value";
+            }
+            else
+            {
+                int regionid = 0;
+                if (!int.TryParse(RegionIDArg.Text,out regionid))
+                {
+                    MessageLabel.Text = "Region ID must be a number";
+                }
+                else
+                {
+                    if (regionid<1)
+                    {
+                        MessageLabel.Text = "Region ID must be a whole number greater than 0";
+                    }
+                    else
+                    {
+                        //anytime you plan on leaving your web app project to go to another project you MUST use trycatch
+                        try
+                        {
+                            //Standard query lookup
+                            //steps
+                            //create a reciving data variable
+                            //create an instance of the controller class you are going to make your request to
+                            //issue your request
+                            //check your results
+                            // a) List<T> use .Count
+                            // b) single record use ==null
+                            //display according to your results
+                            Region info = null;
+                            RegionController sysmgr = new RegionController();
+                            info = sysmgr.Regions_FindByID(regionid);
+                            if (info == null)
+                            {
+                                MessageLabel.Text = "Region id not found";
+                                RegionID.Text = "";
+                                RegionDescription.Text = "";
+                            }
+                            else
+                            {
+                                RegionID.Text = info.RegionID.ToString();
+                                RegionDescription.Text = info.RegionDescription;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            MessageLabel.Text = ex.Message;
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
